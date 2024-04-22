81bceb1cd578fe488898c421381f8d884c956d0f
SDKuma <dk12453@gmail.com>
2024-04-22 18:18:00 +0530

    00012

--- main/admin/includes/footer.php
+++ main/admin/includes/footer.php
@@ -19,7 +19,7 @@
 <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script> -->
 <script src="./../admin/assets/js/bt.min.js" crossorigin="anonymous"></script>
 <script src="assets/js/scripts.js"></script>
-
+<!--//removed cdn links-->
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script> -->
 
 <!-- <script src="assets/demo/chart-area-demo.js"></script>
